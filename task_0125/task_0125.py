2a48eeaaaac55ee100d229cea375f9d07c6c9ec1
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2025-07-08 22:14:33 +0200

    Task 0125 (Python)

---
+++ task_0125/task_0125.py
@@ -0,0 +1,12 @@
+# LeetCode #0125 | Valid Palindrome | [EASY]
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        s = "".join(char.lower() for char in s if char.isalnum())
+        return s == s[::-1]
+        
