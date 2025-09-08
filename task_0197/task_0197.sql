436795d7f47554f337d42871579a8175e7b311e6
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2025-09-08 12:33:38 +0200

    Task 0197 (SQL)

---
+++ task_0197/task_0197.sql
@@ -0,0 +1,7 @@
+# LeetCode #0197 | Rising Temperature | [EASY]
+
+SELECT W1.id
+FROM Weather AS W1
+JOIN Weather AS W2
+ON  DATEDIFF(W1.recordDate, W2.recordDate) = 1
+    AND W1.temperature > W2.temperature 
