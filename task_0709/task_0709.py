a3dd74afd947a02307042f7f82189bcc29fd7802
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2025-09-28 23:45:21 +0200

    Task 0709 (Python)

---
+++ task_0709/task_0709.py
@@ -0,0 +1,11 @@
+# LeetCode #0709 | To Lower Case | [EASY]
+
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        return s.lower()
+        
