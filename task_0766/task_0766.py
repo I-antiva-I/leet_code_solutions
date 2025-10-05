5b9c5e5453f4e8d76045b3f1d631d078d31121d2
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2025-10-05 23:07:47 +0200

    Task 0766 (Python)

---
+++ task_0766/task_0766.py
@@ -0,0 +1,18 @@
+# LeetCode #0766 | Toeplitz Matrix | [EASY]
+
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        
+        r = len(matrix)
+        c = len(matrix[0])
+       
+        for i in range(1, r):
+            for j in range(1, c):
+                if (matrix[i][j] != matrix[i-1][j-1]):
+                    return False
+
+        return True
