2ac9775474857736cf7e2c4daa849b0e71ca764a
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2025-12-12 23:31:16 +0100

    Revert "Task 1045 (SQL)"
    This reverts commit ba71add6810fcc35b0faa424add3b17e8711ab61.

--- task_1045/task_1045.sql
+++
@@ -1,9 +0,0 @@
-# LeetCode #1045 | Customers Who Bought All Products | [MEDIUM]
-
-SELECT DISTINCT C.customer_id FROM Customer AS C
-GROUP BY C.customer_id 
-HAVING COUNT(DISTINCT C.product_key) =
-(
-    SELECT COUNT(P.product_key) FROM Product AS P 
-)
-ORDER BY C.customer_id
--- task_1050/task_1050.sql
+++
@@ -1,6 +0,0 @@
-# LeetCode #1050 | Actors and Directors Who Cooperated At Least Three Times | [EASY]
-
-SELECT actor_id, director_id FROM ActorDirector 
-GROUP BY actor_id, director_id 
-HAVING COUNT(timestamp) > 2
-ORDER BY actor_id, director_id
