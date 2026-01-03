ded0905a9f4e4999f95c35b02a39c32f84e08de8
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2026-01-03 20:39:14 +0100

    Task 2769 (Python)

---
+++ task_2769/task_2769.sql
@@ -0,0 +1,11 @@
+# LeetCode #2769 | Find the Maximum Achievable Number | [EASY]
+
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        
+        return num + t*2
