682c29a12a4075f01540e7a2bd44930b8b944731
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2026-01-04 20:37:22 +0100

    Task 1672 (Python)

---
+++ task_1672/task_1672.py
@@ -0,0 +1,10 @@
+# LeetCode #1672 | Richest Customer Wealth | [EASY]
+
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+
+        return max(map(sum, accounts))
