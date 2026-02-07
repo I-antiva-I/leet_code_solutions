8d4e15feba253dd9dd8369a1bfe273c109ecc368
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2026-02-07 17:44:58 +0100

    Task 1047 (Python)

---
+++ task_1047/task_1047.py
@@ -0,0 +1,19 @@
+# LeetCode #1047 | Remove All Adjacent Duplicates In String | [EASY]
+
+class Solution(object):
+    def removeDuplicates(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        stack = []
+
+        for char in s:
+            if (stack):
+                if (char == stack[-1]):
+                    stack.pop()
+                    continue
+            stack.append(char)
+        
+        return "".join(stack)
